3ea0af6e8e05466c67cda3a8a55da626db4f84c6
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-05-25 18:22:44 -0400

    feat: add google auth

--- src/env.js
+++ src/env.js
@@ -11,8 +11,22 @@ export const env = createEnv({
 			process.env.NODE_ENV === "production"
 				? z.string()
 				: z.string().optional(),
-		AUTH_DISCORD_ID: z.string(),
-		AUTH_DISCORD_SECRET: z.string(),
+		AUTH_DISCORD_ID:
+			process.env.NODE_ENV === "production"
+				? z.string().optional()
+				: z.string(),
+		AUTH_DISCORD_SECRET:
+			process.env.NODE_ENV === "production"
+				? z.string().optional()
+				: z.string(),
+		AUTH_GOOGLE_ID:
+			process.env.NODE_ENV === "production"
+				? z.string()
+				: z.string().optional(),
+		AUTH_GOOGLE_SECRET:
+			process.env.NODE_ENV === "production"
+				? z.string()
+				: z.string().optional(),
 		DATABASE_URL: z.string().url(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
@@ -43,6 +57,8 @@ export const env = createEnv({
 		AUTH_SECRET: process.env.AUTH_SECRET,
 		AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
 		AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+		AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+		AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
 		PROVIDER_CREDENTIALING_ID: process.env.PROVIDER_CREDENTIALING_ID,
