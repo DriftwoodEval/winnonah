d30b3e99f68b3598051278a68835213521350369
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-05-31 23:27:20 -0400

    feat: move login next to alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ErrorAlert } from "~/app/_components/errorAlert";
 import SearchForm from "~/app/_components/searchForm";
 import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
+import { Button } from "./_components/ui/button";
 
 export default async function Home() {
 	const session = await auth();
@@ -12,23 +13,16 @@ export default async function Home() {
 	return (
 		<HydrateClient>
 			<main className="flex min-h-screen flex-col items-center justify-center">
-				<div className="absolute top-0 right-0 m-2">
+				<div className="absolute top-0 right-0 m-2 flex gap-3">
 					<ErrorAlert />
+					<Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+						<Button>{session ? "Sign out" : "Sign in"}</Button>
+					</Link>
 				</div>
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
 					<div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
 						{session?.user && <Clients />}
 						{session?.user && <SearchForm />}
-					</div>
-					<div className="flex flex-col items-center gap-2">
-						<div className="flex flex-col items-center justify-center gap-4">
-							<Link
-								href={session ? "/api/auth/signout" : "/api/auth/signin"}
-								className="dark rounded-full bg-card px-10 py-3 font-semibold text-card-foreground no-underline transition hover:bg-card/20"
-							>
-								{session ? "Sign out" : "Sign in"}
-							</Link>
-						</div>
 					</div>
 				</div>
 			</main>
