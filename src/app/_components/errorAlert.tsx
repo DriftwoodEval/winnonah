ccad92c85fa421509556f273fb459948a4a1c017
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-05-31 23:22:11 -0400

    feat: add errors alert

---
+++ src/app/_components/errorAlert.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "~/app/_components/ui/button";
+import { api } from "~/trpc/react";
+
+export function ErrorAlert() {
+	const asanaErrorsResponse = api.clients.getAsanaErrors.useQuery();
+	const asanaErrors = asanaErrorsResponse.data;
+
+	const districtErrorsResponse = api.clients.getDistrictErrors.useQuery();
+	const districtErrors = districtErrorsResponse.data;
+
+	const errorsLength =
+		(asanaErrors?.length ?? 0) + (districtErrors?.length ?? 0);
+
+	if (errorsLength === 0) {
+		return null;
+	}
+
+	return (
+		<Link href="/errors">
+			<Button className="rounded-full bg-destructive text-foreground">
+				{errorsLength} issues
+			</Button>
+		</Link>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import { Clients } from "~/app/_components/clients";
+import { ErrorAlert } from "~/app/_components/errorAlert";
 import SearchForm from "~/app/_components/searchForm";
 import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
@@ -11,6 +12,9 @@ export default async function Home() {
 	return (
 		<HydrateClient>
 			<main className="flex min-h-screen flex-col items-center justify-center">
+				<div className="absolute top-0 right-0 m-2">
+					<ErrorAlert />
+				</div>
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
 					<div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
 						{session?.user && <Clients />}
