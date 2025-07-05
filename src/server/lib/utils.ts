f18b0743d2382dcea91a80d5ec53a5286fd971db
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-07-05 16:36:48 -0400

    feat: put clients too old for babynet in normal sort

--- src/server/lib/utils.ts
+++ src/server/lib/utils.ts
@@ -4,69 +4,81 @@ import type { clients } from "~/server/db/schema";
 export type Client = InferSelectModel<typeof clients>;
 
 export interface SortedClient extends Client {
-	sortReason?: string;
+  sortReason?: string;
 }
 
 export const getBabyNetClientsAboveAge = (clients: SortedClient[]) => {
-	// Get clients that are older than 2 years:6 months
-	const minAge = new Date();
-	minAge.setFullYear(minAge.getFullYear() - 2);
-	minAge.setMonth(minAge.getMonth() - 6);
+  // Get clients that are older than 2 years:6 months and under 3 years
+  const minAge = new Date();
+  minAge.setFullYear(minAge.getFullYear() - 2);
+  minAge.setMonth(minAge.getMonth() - 6);
 
-	const babynetClientsAboveAge = clients
-		.filter((client) => {
-			return client.dob && new Date(client.dob) < minAge;
-		})
-		.map((client) => ({
-			...client,
-			sortReason: "BabyNet above 2:6",
-		}));
+  const maxAge = new Date();
+  maxAge.setFullYear(maxAge.getFullYear() - 3);
 
-	babynetClientsAboveAge.sort(
-		(a, b) => new Date(a.dob).getTime() - new Date(b.dob).getTime(),
-	);
+  const babynetClientsAboveAge = clients
+    .filter((client) => {
+      return (
+        client.dob &&
+        new Date(client.dob) < maxAge &&
+        new Date(client.dob) > minAge
+      );
+    })
+    .map((client) => ({
+      ...client,
+      sortReason: "BabyNet above 2:6",
+    }));
 
-	return babynetClientsAboveAge;
+  babynetClientsAboveAge.sort(
+    (a, b) => new Date(a.dob).getTime() - new Date(b.dob).getTime()
+  );
+
+  return babynetClientsAboveAge;
 };
 
 export const sortRemainingClientsByAddedDate = (clients: SortedClient[]) => {
-	const sorted = [...clients].map((client) => ({
-		...client,
-		sortReason: "Added date",
-	}));
+  const sorted = [...clients].map((client) => ({
+    ...client,
+    sortReason: "Added date",
+  }));
 
-	sorted.sort(
-		(a, b) =>
-			new Date(a.addedDate ?? "").getTime() -
-			new Date(b.addedDate ?? "").getTime(),
-	);
+  sorted.sort(
+    (a, b) =>
+      new Date(a.addedDate ?? "").getTime() -
+      new Date(b.addedDate ?? "").getTime()
+  );
 
-	return sorted;
+  return sorted;
 };
 
 export const sortClients = (clients: SortedClient[]) => {
-	const clientsWithBabyNet = clients.filter(
-		(client) =>
-			client.primaryInsurance === "BabyNet" ||
-			client.secondaryInsurance === "BabyNet",
-	);
+  const clientsWithBabyNet = clients.filter(
+    (client) =>
+      client.primaryInsurance === "BabyNet" ||
+      client.secondaryInsurance === "BabyNet"
+  );
 
-	const clientsWithBabyNetAboveAge = getBabyNetClientsAboveAge(clientsWithBabyNet);
+  const clientsWithBabyNetAboveAge =
+    getBabyNetClientsAboveAge(clientsWithBabyNet);
 
-	const remainingClientsWithBabyNet = clientsWithBabyNet.filter(
-		(client) =>
-			!clientsWithBabyNetAboveAge.some((clientAboveAge) => clientAboveAge.id === client.id),
-	);
+  const remainingClientsWithBabyNet = clientsWithBabyNet.filter(
+    (client) =>
+      !clientsWithBabyNetAboveAge.some(
+        (clientAboveAge) => clientAboveAge.id === client.id
+      )
+  );
 
-	const remainingClients = clients.filter(
-		(client) =>
-			!clientsWithBabyNet.some((clientWithBabyNet) => clientWithBabyNet.id === client.id),
-	);
+  const remainingClients = clients.filter(
+    (client) =>
+      !clientsWithBabyNet.some(
+        (clientWithBabyNet) => clientWithBabyNet.id === client.id
+      )
+  );
 
-	const sortedRemainingClients = sortRemainingClientsByAddedDate([
-		...remainingClientsWithBabyNet,
-		...remainingClients,
-	]);
+  const sortedRemainingClients = sortRemainingClientsByAddedDate([
+    ...remainingClientsWithBabyNet,
+    ...remainingClients,
+  ]);
 
-	return [...clientsWithBabyNetAboveAge, ...sortedRemainingClients];
+  return [...clientsWithBabyNetAboveAge, ...sortedRemainingClients];
 };
