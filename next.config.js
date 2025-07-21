f02487b5f363270208c8d53461b7ccfb77688c18
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-07-21 18:56:45 -0400

    fix: silence turbopack error

--- next.config.js
+++ next.config.js
@@ -6,21 +6,23 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-	output: "standalone",
-	allowedDevOrigins: [
-		"http://localhost:3000",
-		"https://dev.winnonah.xyz",
-		"*.winnonah.xyz",
-	],
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback.fs = false;
-			config.resolve.fallback.tls = false;
-			config.resolve.fallback.net = false;
-			config.resolve.fallback.child_process = false;
-		}
-		return config;
-	},
+  output: "standalone",
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://dev.winnonah.xyz",
+    "*.winnonah.xyz",
+  ],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+    return config;
+  },
+
+  turbopack: {},
 };
 
 export default config;
