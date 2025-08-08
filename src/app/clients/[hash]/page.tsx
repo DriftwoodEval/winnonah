4e0af7df6671dd8a46addfe1aafc46c7d9f167c1
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-08-08 01:27:20 -0400

    fix: return early when unauthed

--- src/app/clients/[hash]/page.tsx
+++ src/app/clients/[hash]/page.tsx
@@ -1,12 +1,25 @@
 import { Client } from "@components/Client";
+import { auth } from "~/server/auth";
 
 export default async function Page({
 	params,
 }: {
 	params: Promise<{ hash: string }>;
 }) {
+	const session = await auth();
 	const parameters = await params;
 	const hash = parameters.hash;
+
+	if (!session) {
+		return (
+			<main className="flex min-h-screen items-center justify-center">
+				<h1 className="font-bold text-2xl">
+					You must be logged in to view this page.
+				</h1>
+			</main>
+		);
+	}
+
 	return (
 		<main className="flex min-h-screen items-center justify-center">
 			<Client hash={hash} />
