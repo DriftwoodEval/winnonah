1c136ae8559b38673b41888eadaaae16a43f2ac6
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-08-16 00:43:09 -0400

    feat: add card layouts on tables for small screens

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -22,13 +22,15 @@ export default async function Home() {
 			<div className="mx-10 my-10 flex w-full flex-col gap-6">
 				<h1 className="font-bold text-2xl">Settings</h1>
 				<Tabs defaultValue="users">
-					<TabsList>
+					<TabsList className="w-full sm:w-1/2">
 						<TabsTrigger value="users">Users</TabsTrigger>
 						<TabsTrigger value="evaluators">Evaluators</TabsTrigger>
 					</TabsList>
 					<TabsContent value="users">
-						<UsersTable />
-						<InvitesTable />
+						<div className="flex flex-col gap-8">
+							<UsersTable />
+							<InvitesTable />
+						</div>
 					</TabsContent>
 					<TabsContent value="evaluators">
 						<EvaluatorsTable />
