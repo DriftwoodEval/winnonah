a514368cba83dbc4b7330cf4f310c24d640b656a
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-08-22 23:57:34 -0400

    fix: middleware on issueslist

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,6 +1,19 @@
 import { IssuesList } from "@components/issues/issuesList";
+import { auth } from "~/server/auth";
 
 export default async function Page() {
+	const session = await auth();
+
+	if (!session) {
+		return (
+			<main className="flex min-h-screen items-center justify-center">
+				<h1 className="font-bold text-2xl">
+					You must be logged in to view this page.
+				</h1>
+			</main>
+		);
+	}
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<IssuesList />
