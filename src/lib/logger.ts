ed7cf0cfc944863e88744c49062a84e6aca27b6a
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-11-24 23:02:54 -0500

    feat: logfile

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,7 +3,16 @@ import pino, { type Logger } from "pino";
 export const logger: Logger =
   process.env.NODE_ENV === "production"
     ? // JSON in production
-      pino({ base: null, level: "info" })
+      pino({
+        base: null,
+        transport: {
+          targets: [
+            { target: "pino/file", options: { destination: "logs/debug.log" } },
+            { target: "pino/file", options: { destination: 1 } },
+          ],
+        },
+        level: "debug",
+      })
     : // Pretty in development
       pino({
         base: null,
