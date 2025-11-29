bfea9b66efdc1a356762813c4e0c17337623381f
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-11-28 20:51:20 -0500

    feat: add needs ifsp downloaded issue

--- src/app/_components/issues/issuesList.tsx
+++ src/app/_components/issues/issuesList.tsx
@@ -582,6 +582,8 @@ export function IssuesList() {
 		api.clients.getDropList.useQuery();
 	const { data: autismStops, isLoading: isLoadingAutismStops } =
 		api.clients.getAutismStops.useQuery();
+	const { data: needsIFSPDownloaded } =
+		api.clients.getNeedsIFSPDownloaded.useQuery();
 	const { data: noteOnlyClients, isLoading: isLoadingNoteOnlyClients } =
 		api.clients.getNoteOnlyClients.useQuery();
 	const { data: noDriveIds, isLoading: isLoadingNoDriveIds } =
@@ -682,6 +684,12 @@ export function IssuesList() {
 			{isLoadingAutismStops && <IssueListSkeleton />}
 			{!isLoadingAutismStops && autismStops && autismStops.length !== 0 && (
 				<IssueList clients={autismStops} title="Autism Stops" />
+			)}
+			{needsIFSPDownloaded && needsIFSPDownloaded.length !== 0 && (
+				<IssueList
+					clients={needsIFSPDownloaded}
+					title="Needs IFSP Downloaded"
+				/>
 			)}
 			{isLoadingNoteOnlyClients && <IssueListSkeleton />}
 			{!isLoadingNoteOnlyClients &&
