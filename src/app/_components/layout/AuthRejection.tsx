2e6489b4b4496fd4824de6f93b94b7f73559839c
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-12-19 17:54:44 -0500

    fix: center auth rejection

--- src/app/_components/layout/AuthRejection.tsx
+++ src/app/_components/layout/AuthRejection.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 export function AuthRejection() {
 	return (
 		<main className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-6">
-			<h1 className="font-bold text-2xl">
+			<h1 className="text-center font-bold text-2xl">
 				You must be logged in to view this page.
 			</h1>
 
