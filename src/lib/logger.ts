41ae60d81fbdb5c5aca86426bbb9a8d27be97e74
Jeremiah <44993244+jechton@users.noreply.github.com>
2026-02-27 19:38:19 -0500

    fix: update logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,24 +1,24 @@
 import pino, { type Logger } from "pino";
 
-export const logger: Logger =
-	process.env.NODE_ENV === "production"
-		? // JSON in production
-			pino({
-				base: null,
-				transport: {
-					targets: [
-						{ target: "pino/file", options: { destination: "logs/debug.log" } },
-						{ target: "pino/file", options: { destination: 1 } },
-					],
+const isProd = process.env.NODE_ENV === "production";
+
+export const logger: Logger = isProd
+	? pino(
+			{ level: "debug", base: null },
+			pino.multistream([
+				{ stream: process.stdout },
+				{
+					stream: pino.destination({
+						dest: "/app/logs/debug.log",
+						sync: false,
+					}),
 				},
-				level: "debug",
-			})
-		: // Pretty in development
-			pino({
-				base: null,
-				transport: {
-					target: "pino-pretty",
-					options: { colorize: true, singleLine: true },
-				},
-				level: "debug",
-			});
+			]),
+		)
+	: pino({
+			transport: {
+				target: "pino-pretty",
+				options: { colorize: true, singleLine: true },
+			},
+			level: "debug",
+		});
