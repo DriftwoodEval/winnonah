f680aeb22f595ddaf9ec78c8883556f5a47bd31b
Jeremiah <44993244+jechton@users.noreply.github.com>
2026-05-07 00:39:10 -0400

    feat: add most of spanish script to referral tab

--- src/app/_components/client/ReferralTab.tsx
+++ src/app/_components/client/ReferralTab.tsx
@@ -188,6 +188,7 @@ export function ReferralTab({ client, readOnly }: ReferralTabProps) {
 	const userName = session?.user?.name?.split(" ")[0] ?? "(Your Name)";
 	const asdAdhdValue = client.asdAdhd ?? "(Diagnosis)";
 	const isAdhd = client.asdAdhd === "ADHD";
+	const isSpanish = client.language === "Spanish";
 
 	const ageInMonths = differenceInMonths(new Date(), new Date(client.dob));
 	const ageInYears = differenceInYears(new Date(), new Date(client.dob));
@@ -364,19 +365,39 @@ export function ReferralTab({ client, readOnly }: ReferralTabProps) {
 				<CardContent className="space-y-6">
 					<div className="flex flex-col gap-4">
 						<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
-							<p>
-								This is {userName} from Driftwood Evaluation Center. We received
-								your referral and wanted to ask you some questions to get
-								started on our process.
-							</p>
-							<p>
-								I just want to confirm you and/or your pediatrician have
-								concerns about potential{" "}
-								{asdAdhdValue
-									.replace("ASD", "autism")
-									.replace("LD", "learning disability")}
-								.
-							</p>
+							{isSpanish ? (
+								<p>
+									Le habla Barbara, de Driftwood Evaluation Center. Hemos
+									recibido su remisión y queríamos hacerle algunas preguntas
+									para iniciar nuestro proceso.
+								</p>
+							) : (
+								<p>
+									This is {userName} from Driftwood Evaluation Center. We
+									received your referral and wanted to ask you some questions to
+									get started on our process.
+								</p>
+							)}
+
+							{isSpanish ? (
+								<p>
+									Solo quiero confirmar si usted y/o el pediatra tienen
+									inquietudes sobre la posibilidad de{" "}
+									{asdAdhdValue
+										.replace("ASD", "Autismo")
+										.replace("LD", "discapacidad de aprendizaje")}
+									.
+								</p>
+							) : (
+								<p>
+									I just want to confirm you and/or your pediatrician have
+									concerns about potential{" "}
+									{asdAdhdValue
+										.replace("ASD", "autism")
+										.replace("LD", "learning disability")}
+									.
+								</p>
+							)}
 						</div>
 
 						{isUnder3 && (
@@ -444,7 +465,11 @@ export function ReferralTab({ client, readOnly }: ReferralTabProps) {
 						{showSchoolQuestion && (
 							<div className="space-y-4">
 								<div className="rounded-lg bg-muted p-4 text-sm">
-									<p>Does the child go to charter / private school?</p>
+									{isSpanish ? (
+										<p>¿Asiste el niño a una escuela chárter o privada?</p>
+									) : (
+										<p>Does the child go to charter / private school?</p>
+									)}
 								</div>
 								<div className="space-y-4">
 									<div className="space-y-2">
@@ -479,201 +504,263 @@ export function ReferralTab({ client, readOnly }: ReferralTabProps) {
 						)}
 					</div>
 
-					<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
-						<p>
-							We would like to set you up in our{" "}
-							{client.taHash ? (
-								<Link
-									className="underline"
-									href={`https://api.portal.therapyappointment.com/n/client/${client.taHash}`}
-									target="_blank"
-								>
-									Patient Portal
-								</Link>
-							) : (
-								<span>Patient Portal</span>
-							)}{" "}
-							. To do that, we need to send you an email.{" "}
-							{client.email
-								? `I want to confirm that your email is "${client.email}"?`
-								: "Can I please have your email address?"}
-						</p>
-					</div>
-
-					<div className="space-y-4">
-						<div className="flex flex-col gap-2 rounded-lg bg-accent p-4 text-accent-foreground text-sm">
-							<div className="flex w-full items-center justify-between">
-								<h3 className="font-semibold">Invite to TherapyAppointment</h3>
-								<div className="flex items-center gap-2">
-									{client.taHash && (
+					{!isSpanish ? (
+						<>
+							<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
+								<p>
+									We would like to set you up in our{" "}
+									{client.taHash ? (
 										<Link
+											className="underline"
 											href={`https://api.portal.therapyappointment.com/n/client/${client.taHash}`}
 											target="_blank"
 										>
-											<Armchair height="16" width="16" />
+											Patient Portal
 										</Link>
+									) : (
+										<span>Patient Portal</span>
+									)}{" "}
+									. To do that, we need to send you an email.{" "}
+									{client.email
+										? `I want to confirm that your email is "${client.email}"?`
+										: "Can I please have your email address?"}
+								</p>
+							</div>
+
+							<div className="space-y-4">
+								<div className="flex flex-col gap-2 rounded-lg bg-accent p-4 text-accent-foreground text-sm">
+									<div className="flex w-full items-center justify-between">
+										<h3 className="font-semibold">
+											Invite to TherapyAppointment
+										</h3>
+										<div className="flex items-center gap-2">
+											{client.taHash && (
+												<Link
+													href={`https://api.portal.therapyappointment.com/n/client/${client.taHash}`}
+													target="_blank"
+												>
+													<Armchair height="16" width="16" />
+												</Link>
+											)}
+										</div>
+									</div>
+									<p>
+										Go to the client's TherapyAppointment page by clicking the
+										chair icon. If the client's email is different, update it in
+										TherapyAppointment. Then, click{" "}
+										<span className="font-bold">Send Portal Invitation</span>.
+									</p>
+									<p className="whitespace-pre-wrap"></p>
+								</div>
+
+								<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
+									<p>
+										I'd like you to create your account while we're on the phone
+										together. You should receive an email from
+										TherapyAppointment, please click on the link in that email
+										to sign into the Patient Portal. You will use the client's
+										date of birth to set up your account. Let me know if you
+										have any questions with that process.
+									</p>
+
+									<p>
+										Once you're in the portal, there will be documents to sign.
+										They usually will pop up as part of the sign-up process.
+										Please complete them so we can move forward.
+									</p>
+
+									{showSchoolQuestion && (
+										<p>
+											We will request records from the school district and then
+											be in touch to discuss next steps.
+										</p>
+									)}
+
+									{isAdhd ? (
+										<>
+											<p>
+												We will send you a questionnaire through the portal
+												soon. Look for a new message, which is usually in the
+												upper right hand corner of the screen. You will also get
+												an email telling you that you have a new message.
+											</p>
+											<p>
+												We will contact you after this is done to set up the
+												appointment.
+											</p>
+											<p>
+												This appointment takes approximately 1 hour and can be
+												done either virtually or in person.
+											</p>
+										</>
+									) : (
+										<>
+											<p>
+												Soon, we will send you questionnaires to complete. Look
+												for a new message, which is usually in the upper right
+												hand corner of the screen. You will also get an email
+												telling you that you have a new message. Generally, we
+												will schedule an intake appointment via video
+												call/Telehealth. That appointment will tell us if we are
+												moving ahead with the evaluation. If so, we will request
+												approval from insurance before sending more
+												questionnaires in order to get the in-person
+												comprehensive evaluation scheduled.
+											</p>
+											<p>
+												This process takes approximately 4-6 months to complete.
+												It is very important that we have your cooperation with
+												this process; if we have to wait for you to complete
+												something that delays everything. Please be patient with
+												us as we help as many people as possible across the
+												state.
+											</p>
+										</>
 									)}
 								</div>
+
+								{isAdhd && (
+									<div className="space-y-2">
+										<Label className="font-semibold">Preference?</Label>
+										<RadioGroup
+											className="flex flex-wrap gap-4"
+											disabled={
+												isReadOnly ||
+												updateClientMutation.isPending ||
+												!can("clients:referral:fillout")
+											}
+											onValueChange={(value) => {
+												setLocationPreference(value);
+												handleReferralDataChange({ locationPreference: value });
+											}}
+											value={locationPreference}
+										>
+											<div className="flex items-center space-x-2">
+												<RadioGroupItem id="virtual" value="virtual" />
+												<Label className="font-normal" htmlFor="virtual">
+													Virtual
+												</Label>
+											</div>
+											<div className="flex items-center space-x-2">
+												<RadioGroupItem id="in-person" value="in-person" />
+												<Label className="font-normal" htmlFor="in-person">
+													In Person
+												</Label>
+											</div>
+										</RadioGroup>
+									</div>
+								)}
+								<div className="space-y-2">
+									<Label className="font-semibold" htmlFor="otherNotes">
+										Other Notes
+									</Label>
+									<Textarea
+										disabled={
+											isReadOnly ||
+											updateClientMutation.isPending ||
+											!can("clients:referral:fillout")
+										}
+										id="otherNotes"
+										onBlur={() => {
+											if (
+												otherNotes !== (client.referralData?.otherNotes ?? "")
+											) {
+												handleReferralDataChange({ otherNotes });
+											}
+										}}
+										onChange={(e) => setOtherNotes(e.target.value)}
+										placeholder="..."
+										value={otherNotes}
+									/>
+								</div>
 							</div>
-							<p>
-								Go to the client's TherapyAppointment page by clicking the chair
-								icon. If the client's email is different, update it in
-								TherapyAppointment. Then, click{" "}
-								<span className="font-bold">Send Portal Invitation</span>.
-							</p>
-							<p className="whitespace-pre-wrap"></p>
-						</div>
 
-						<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
-							<p>
-								I'd like you to create your account while we're on the phone
-								together. You should receive an email from TherapyAppointment,
-								please click on the link in that email to sign into the Patient
-								Portal. You will use the client's date of birth to set up your
-								account. Let me know if you have any questions with that
-								process.
-							</p>
-
-							<p>
-								Once you're in the portal, there will be documents to sign. They
-								usually will pop up as part of the sign-up process. Please
-								complete them so we can move forward.
-							</p>
-
-							{showSchoolQuestion && (
-								<p>
-									We will request records from the school district and then be
-									in touch to discuss next steps.
+							<div className="flex flex-col gap-2 rounded-lg bg-accent p-4 text-accent-foreground text-sm">
+								<div className="flex w-full items-center justify-between">
+									<h3 className="font-semibold">TherapyAppointment Message</h3>
+									<div className="flex items-center gap-2">
+										<Button
+											className="h-8 w-8"
+											onClick={() => {
+												void navigator.clipboard.writeText(TA_MESSAGE);
+												toast.success("Copied to clipboard");
+											}}
+											size="icon"
+											variant="ghost"
+										>
+											<Copy className="h-4 w-4" />
+										</Button>
+										{client.taHash && (
+											<Link
+												href={`https://api.portal.therapyappointment.com/n/client/${client.taHash}`}
+												target="_blank"
+											>
+												<Armchair height="16" width="16" />
+											</Link>
+										)}
+									</div>
+								</div>
+								<p className="font-bold">
+									Copy the message below by clicking the two boxes in the top
+									right. Go to the client's TherapyAppointment page by clicking
+									the chair icon and send them the following message:
 								</p>
-							)}
-
-							{isAdhd ? (
+								<p className="whitespace-pre-wrap">{TA_MESSAGE}</p>
+							</div>
+						</>
+					) : (
+						<div className="flex flex-col gap-2 rounded-lg bg-muted p-4 text-sm">
+							{!isAdhd ? (
 								<>
 									<p>
-										We will send you a questionnaire through the portal soon.
-										Look for a new message, which is usually in the upper right
-										hand corner of the screen. You will also get an email
-										telling you that you have a new message.
+										Pronto le enviaremos por correo electrónico algunos
+										formularios de consentimiento y relacionados con la
+										normativa HIPAA para que los firme; estos estarán en
+										español. Una vez firmados, le enviaremos un cuestionario, el
+										cual consiste en una lista de preguntas que deberá
+										responder, también en español. Una vez completados, se le
+										asignará un intérprete. Esta persona nos ayudará a programar
+										su cita en nuestra oficina. Por lo general, en esa cita
+										determinaremos si vamos a proceder con la evaluación. De ser
+										así, solicitaremos el permiso de su compañía de seguros
+										médico. Una vez recibida dicho permiso, le enviaremos más
+										cuestionarios. Cuando estos estén completos, nuestro
+										intérprete se comunicará con usted para programar la
+										evaluación completa en persona.
 									</p>
+									{showSchoolQuestion && (
+										<p>Solicitaremos los expedientes al distrito escolar.</p>
+									)}
 									<p>
-										We will contact you after this is done to set up the
-										appointment.
-									</p>
-									<p>
-										This appointment takes approximately 1 hour and can be done
-										either virtually or in person.
+										Este proceso tarda aproximadamente entre 4 y 6 meses en
+										completarse. Es fundamental contar con su colaboración
+										durante este proceso; si témenos que esperar que usted
+										complete alguna tarea, esto retrasará todo el procedimiento.
+										Le pedimos paciencia, ya que nuestra intención es ayudar al
+										mayor número posible de personas en todo el estado.
 									</p>
 								</>
 							) : (
-								<>
-									<p>
-										Soon, we will send you questionnaires to complete. Look for
-										a new message, which is usually in the upper right hand
-										corner of the screen. You will also get an email telling you
-										that you have a new message. Generally, we will schedule an
-										intake appointment via video call/Telehealth. That
-										appointment will tell us if we are moving ahead with the
-										evaluation. If so, we will request approval from insurance
-										before sending more questionnaires in order to get the
-										in-person comprehensive evaluation scheduled.
-									</p>
-									<p>
-										This process takes approximately 4-6 months to complete. It
-										is very important that we have your cooperation with this
-										process; if we have to wait for you to complete something
-										that delays everything. Please be patient with us as we help
-										as many people as possible across the state.
-									</p>
-								</>
+								<p>
+									Pronto le enviaremos por correo electrónico algunos
+									formularios de consentimiento y relacionados con la normativa
+									HIPAA para que nos los firme; estos estarán en español. Una
+									vez firmados, le enviaremos un cuestionario —que consiste en
+									una lista de preguntas para responder, también en español. Una
+									vez completado, se le asignará un intérprete, quien nos
+									ayudará a programar su cita en nuestra oficina.{" "}
+									{showSchoolQuestion && (
+										<span>
+											Solicitaremos los expedientes al distrito escolar.
+										</span>
+									)}{" "}
+									Nos pondremos en contacto con Usted una vez finalizado este
+									proceso para concertar la cita. Esta cita tiene una duración
+									aproximada de una hora.
+								</p>
 							)}
 						</div>
-
-						{isAdhd && (
-							<div className="space-y-2">
-								<Label className="font-semibold">Preference?</Label>
-								<RadioGroup
-									className="flex flex-wrap gap-4"
-									disabled={
-										isReadOnly ||
-										updateClientMutation.isPending ||
-										!can("clients:referral:fillout")
-									}
-									onValueChange={(value) => {
-										setLocationPreference(value);
-										handleReferralDataChange({ locationPreference: value });
-									}}
-									value={locationPreference}
-								>
-									<div className="flex items-center space-x-2">
-										<RadioGroupItem id="virtual" value="virtual" />
-										<Label className="font-normal" htmlFor="virtual">
-											Virtual
-										</Label>
-									</div>
-									<div className="flex items-center space-x-2">
-										<RadioGroupItem id="in-person" value="in-person" />
-										<Label className="font-normal" htmlFor="in-person">
-											In Person
-										</Label>
-									</div>
-								</RadioGroup>
-							</div>
-						)}
-						<div className="space-y-2">
-							<Label className="font-semibold" htmlFor="otherNotes">
-								Other Notes
-							</Label>
-							<Textarea
-								disabled={
-									isReadOnly ||
-									updateClientMutation.isPending ||
-									!can("clients:referral:fillout")
-								}
-								id="otherNotes"
-								onBlur={() => {
-									if (otherNotes !== (client.referralData?.otherNotes ?? "")) {
-										handleReferralDataChange({ otherNotes });
-									}
-								}}
-								onChange={(e) => setOtherNotes(e.target.value)}
-								placeholder="..."
-								value={otherNotes}
-							/>
-						</div>
-					</div>
-
-					<div className="flex flex-col gap-2 rounded-lg bg-accent p-4 text-accent-foreground text-sm">
-						<div className="flex w-full items-center justify-between">
-							<h3 className="font-semibold">TherapyAppointment Message</h3>
-							<div className="flex items-center gap-2">
-								<Button
-									className="h-8 w-8"
-									onClick={() => {
-										void navigator.clipboard.writeText(TA_MESSAGE);
-										toast.success("Copied to clipboard");
-									}}
-									size="icon"
-									variant="ghost"
-								>
-									<Copy className="h-4 w-4" />
-								</Button>
-								{client.taHash && (
-									<Link
-										href={`https://api.portal.therapyappointment.com/n/client/${client.taHash}`}
-										target="_blank"
-									>
-										<Armchair height="16" width="16" />
-									</Link>
-								)}
-							</div>
-						</div>
-						<p className="font-bold">
-							Copy the message below by clicking the two boxes in the top right.
-							Go to the client's TherapyAppointment page by clicking the chair
-							icon and send them the following message:
-						</p>
-						<p className="whitespace-pre-wrap">{TA_MESSAGE}</p>
-					</div>
+					)}
 
 					{isNeedsReview &&
 						can("clients:referral:pushtopunch") &&
